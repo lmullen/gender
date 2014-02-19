5d1b9a0e8cfae469716b4f9e1f988701406cf38f
Lincoln Mullen <lincoln@lincolnmullen.com>
2014-02-19 15:57:29 -0500

    Added checks for the values of year

--- R/gender.R
+++ R/gender.R
@@ -1,6 +1,13 @@
 encode_gender <- function(first_names, year = c(1970, 2012), method = "ssa") {
   if (method == "ssa") {
-    encode_gender_ssa(first_names, year)
+    # Check for errors in the year argument
+    if (length(year) > 2) {
+      stop("Year should be a numeric vector with no more than two values.")
+    } else if (year[1] > year[2]) {
+      stop("The first value for year should be smaller than the second value.")
+    } else {
+      encode_gender_ssa(first_names, year)
+    }
   } else if (method == "corpus") {
     if (!missing(year)) {
       warning("The year is not taken into account with the corpus method.") 
