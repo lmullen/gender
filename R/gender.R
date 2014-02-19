fd185a9c6f2faf2ec28b4cd57b91dc03e9fb9d75
Lincoln Mullen <lincoln@lincolnmullen.com>
2014-02-18 23:49:19 -0500

    Better warnings when arguments aren't what we expect

--- R/gender.R
+++ R/gender.R
@@ -2,9 +2,11 @@ encode_gender <- function(first_names, year = c(1970, 2012), method = "ssa") {
   if (method == "ssa") {
     encode_gender_ssa(first_names, year)
   } else if (method == "corpus") {
+    if (!missing(year)) {
+      warning("The year is not taken into account with the corpus method.") 
+    }
     encode_gender_corpus(first_names)
   } else {
-    warning("Method ", method, " is not recognized. Try ?encode_gender for help.")
+    stop("Method ", method, " is not recognized. Try ?encode_gender for help.")
   }
 }
-
