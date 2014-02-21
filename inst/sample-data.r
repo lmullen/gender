4f920175f767a66613b89b8282810efcaad262a8
Lincoln Mullen <lincoln@lincolnmullen.com>
2014-02-21 16:58:39 -0500

    More thoroughly test Kantrowitz

--- inst/sample-data.r
+++ inst/sample-data.r
@@ -1,7 +1,11 @@
+require(dplyr)
+
 test_data <- data.frame(
-  name = c("john", "john", "jane", "jane", "madison", "madison", "madison", "zzz"),
-  year = c(1890, 1990, 1890, 1990, 1890, 1980, 1990, 1600),
+  name = c("john", "john", "jane", "jane", "madison", "madison", "madison", "zzz",
+           "lindsay", "lindsay", "lindsay"),
+  year = c(1890, 1990, 1890, 1990, 1890, 1980, 1990, 1600, 1600, 1950, 2015),
   stringsAsFactors = FALSE
-  )
+  ) %.%
+  arrange(name)
 
 test_list <- list(name = "madison", year = "1900")
