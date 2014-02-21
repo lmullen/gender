b8e9739827df1d2759736c9add68f63140a5c50f
Lincoln Mullen <lincoln@lincolnmullen.com>
2014-02-21 14:47:02 -0500

    Basic testing framework

---
+++ inst/tests/argument-validation.r
@@ -0,0 +1,8 @@
+context("Checking arguments to functions")
+
+test_that("method kantrowitz warns if years are provided", {
+  expect_that(encode_gender(data = "julie", years = c(1880,1900), method = 
+                            "kantrowitz"),
+              gives_warning("The year is not taken into account"))
+
+})
---
+++ inst/tests/run-all.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(mypackage)
+
+test_package("gender")
