f5fbeb5e23ad116c692a253f82f69daaedf48938
Lincoln Mullen <lincoln@lincolnmullen.com>
2014-08-29 10:19:53 -0400

    Warn if genderize method uses years

--- R/gender.R
+++ R/gender.R
@@ -85,6 +85,7 @@ gender <- function(name, years = c(1932, 2012), method = "ssa",
     }
     gender_ipums_usa(name = name, years = years, certainty = certainty)
   } else if (method == "genderize") {
+    if (!missing(years)) warning("Genderize method does not account for year.")
     gender_genderize(name = name)
   } else {
     stop("Method ", method, " is not recognized. Try ?gender for help.")
