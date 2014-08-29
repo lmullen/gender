7268f6afff289bc64a61c72ec19e1b2328b8f4c4
Lincoln Mullen <lincoln@lincolnmullen.com>
2014-08-28 23:09:45 -0400

    Genderize takes multiple names

--- R/gender-genderize.R
+++ R/gender-genderize.R
@@ -3,9 +3,19 @@ gender_genderize <- function(name) {
   require(jsonlite)
   require(magrittr)
   endpoint <- "http://api.genderize.io"
-  r <- GET(endpoint, query = list(name = name))
-  stop_for_status(r)
-  content(r, as = "text") %>%
-    jsonlite::fromJSON(., simplifyVector = FALSE)
+
+  apply_genderize <- function(n, country = country, lang = lang) {
+    r <- GET(endpoint, query = list(name = n))
+    stop_for_status(r)
+    content(r, as = "text") %>%
+      jsonlite::fromJSON(., simplifyVector = FALSE)
+  }
+
+  if (length(name) == 1) {
+    return(as.list(apply_genderize(name)))
+  } else {
+    return(as.list(lapply(name, apply_genderize)))
+  }
+
 }
 
