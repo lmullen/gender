edd7953b5c0ea1756d9066a856981feb4ffb5c18
Lincoln Mullen <lincoln@lincolnmullen.com>
2014-09-12 13:46:51 -0400

    Function to install genderdata if necessary

---
+++ R/install-genderdata-package.R
@@ -0,0 +1,14 @@
+#' Install datasets for predicting gender
+#'
+#' If the genderdata package is not installed, install it from GitHub using
+#' devtools. If it is not up to date, reinstall it.
+install_genderdata_package <- function() {
+  current_genderdata_version <- "0.1"
+  if (! "genderdata" %in% installed.packages()) {
+    message("Installing datasets for predicting gender from GitHub")
+    devtools::install_github("lmullen/gender-data-pkg")
+  } else if (packageVersion("genderdata") < current_genderdata_version) {
+    message("Updating dataset for predicting gender from GitHub")
+    devtools::install_github("lmullen/gender-data-pkg")
+  }
+}
