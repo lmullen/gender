d55ae74effec3c00a944dff5b502c3605b1e6bb6
Lincoln Mullen <lincoln@lincolnmullen.com>
2014-09-13 03:33:30 -0400

    Specify namespace in onLoad function

--- R/install-genderdata-package.R
+++ R/install-genderdata-package.R
@@ -4,11 +4,11 @@
 #' devtools. If it is not up to date, reinstall it.
 #' @export
 install_genderdata_package <- function() {
-  current_genderdata_version <- "0.1"
-  if (! "genderdata" %in% installed.packages()) {
+  genderdata_version <- "0.1"
+  if (! "genderdata" %in% utils::installed.packages()) {
     message("Installing datasets for predicting gender from GitHub")
     devtools::install_github("lmullen/gender-data-pkg")
-  } else if (packageVersion("genderdata") < current_genderdata_version) {
+  } else if (utils::packageVersion("genderdata") < genderdata_version) {
     message("Updating dataset for predicting gender from GitHub")
     devtools::install_github("lmullen/gender-data-pkg")
   }
