279b87797ce769e61fb9d940f59a2cc47164cb14
Lincoln Mullen <lincoln@lincolnmullen.com>
2019-11-05 10:20:05 -0500

    Run all tests on CI

--- tests/run-all.R
+++ tests/run-all.R
@@ -1,6 +1,6 @@
 library(testthat)
 if (requireNamespace("genderdata", quietly = TRUE)) {
-  test_check("gender", filter = "demo")
+  test_check("gender")
 } else {
   test_check("gender", filter = "demo")
 }
