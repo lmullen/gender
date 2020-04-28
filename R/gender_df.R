e3b48e9fae8c9d77de6b52f3f7547bd04d24e4ea
Hadley Wickham <h.wickham@gmail.com>
2020-04-28 13:55:49 -0500

    Use tibble() instead of data_frame()

--- R/gender_df.R
+++ R/gender_df.R
@@ -26,7 +26,7 @@
 #'   years.
 #' @examples
 #' library(dplyr)
-#' demo_df <- data_frame(names = c("Hillary", "Hillary", "Hillary",
+#' demo_df <- tibble(names = c("Hillary", "Hillary", "Hillary",
 #'                                 "Madison", "Madison"),
 #'                       birth_year = c(1930, 2000, 1930, 1930, 2000),
 #'                       min_year = birth_year - 1,
--- tests/testthat/sample-data.r
+++ tests/testthat/sample-data.r
@@ -4,7 +4,7 @@ sample_names_data <- c("jane", "jane", "madison", "madison")
 sample_years_ssa  <- c(rep(c(1930, 2010), 2))
 sample_years_ipums  <- c(rep(c(1830, 1880), 2))
 
-sample_names_df <- data_frame(names = sample_names_data,
+sample_names_df <- tibble(names = sample_names_data,
                               years = sample_years_ssa)
 
 sample_names_df_big <- bind_rows(sample_names_df, sample_names_df)
