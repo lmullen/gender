c6e0f97365786aad418a13ac0c79d18b62a5488c
Fangzhou Xie <45321577+mark-fangzhou-xie@users.noreply.github.com>
2020-07-08 10:09:11 -0400

    Fixes installation of genderdata package
    fix: https://github.com/ropensci/gender/issues/63

--- R/install-genderdata-package.R
+++ R/install-genderdata-package.R
@@ -17,10 +17,11 @@ check_genderdata_package <- function() {
 #' Install the genderdata package after checking with the user
 #' @export
 install_genderdata_package <- function() {
-  instructions <- paste(" Please try installing the package for yourself",
-                        "using the following command: \n",
-  "    install.packages(\"genderdata\", repos = \"https://dev.ropensci.org\",",
-  "type = \"source\")")
+  instructions <- paste(
+    " Please try installing the package for yourself",
+    "using the following command: \n",
+    "    install.packages(\"genderdata\", repos = \"http://packages.ropensci.org\")"
+  )
 
   error_func <- function(e) {
     stop(paste("Failed to install the genderdata package.\n", instructions))
@@ -28,16 +29,22 @@ install_genderdata_package <- function() {
 
   if (interactive()) {
     input <- utils::menu(c("Yes", "No"),
-                         title = "Install the genderdata package?")
+                         title = "Install the genderdata package?"
+    )
     if (input == 1) {
       message("Installing the genderdata package.")
-      tryCatch(utils::install.packages("genderdata",
-                                       repos = "https://dev.ropensci.org",
-                                       type = "source"),
-               error = error_func, warning = error_func)
+      tryCatch(
+        utils::install.packages("genderdata",
+          repos = "http://packages.ropensci.org"
+        ),
+        error = error_func,
+        warning = error_func
+      )
     } else {
-      stop(paste("The genderdata package is necessary for that method.\n",
-                 instructions))
+      stop(paste(
+        "The genderdata package is necessary for that method.\n",
+        instructions
+      ))
     }
   } else {
     stop(paste("Failed to install the genderdata package.\n", instructions))
